d6d3f6c01f7a2b80e4a0f4373a82840c91e72954
Tycho <mach.muell.tonne@gmail.com>
2025-02-01 22:21:31 +0100

    Movement

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame 
+import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     print ("Starting asteroids!")
@@ -10,14 +12,21 @@ def main():
     print (f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-
+    game_clock = pygame.time.Clock()
+    dt = 0.
+   
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         screen.fill((0,0,0))
-        pygame.display.flip()
+        dt = game_clock.tick(60)/1000
+        player.draw(screen)
+        player.update(dt)
         
 
+        
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
